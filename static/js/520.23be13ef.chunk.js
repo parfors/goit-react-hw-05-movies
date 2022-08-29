7aee847e5bef2cf3449d2205a7c3e287ce5a024b
parfors <94446043+parfors@users.noreply.github.com>
2022-08-29 07:47:20 +0000

    Deploying to gh-pages from @ parfors/goit-react-hw-05-movies@a6a8f096262dfc0671347bfb0bded2bc9a0d52d1 🚀

---
+++ static/js/520.23be13ef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,t,n){n.r(t);var r=n(2884),a=n(5001),c=n(6871),o=n(184);t.default=function(){var e=(0,c.s0)();return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)(r.ButtonBackStyled,{onClick:function(){e("/")},children:"Back to Home"}),(0,o.jsx)(a.HeaderStyled,{children:"404 Page is not found"})]})}}}]);
+//# sourceMappingURL=520.23be13ef.chunk.js.map
